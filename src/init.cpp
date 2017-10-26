7da8e94716c70195ff443817d732eb0cbbaaeb21
j-ackerman <jackerman91@gmail.com>
2017-10-25 21:45:14 -0700

    genesis

--- src/init.cpp
+++ src/init.cpp
@@ -525,8 +525,8 @@ std::string HelpMessage(HelpMessageMode mode)
 
 std::string LicenseInfo()
 {
-    const std::string URL_SOURCE_CODE = "<https://github.com/litecoin-project/litecoin>";
-    const std::string URL_WEBSITE = "<https://litecoin.org>";
+    const std::string URL_SOURCE_CODE = "<https://github.com/bitcoinlite-project/bitcoinlite>";
+    const std::string URL_WEBSITE = "<https://bitcoinlite.org>";
 
     return CopyrightHolders(strprintf(_("Copyright (C) %i-%i"), 2011, COPYRIGHT_YEAR) + " ") + "\n" +
            "\n" +
@@ -815,7 +815,7 @@ void InitLogging()
     fLogIPs = gArgs.GetBoolArg("-logips", DEFAULT_LOGIPS);
 
     LogPrintf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-    LogPrintf("Litecoin version %s\n", FormatFullVersion());
+    LogPrintf("BitcoinLite version %s\n", FormatFullVersion());
 }
 
 namespace { // Variables internal to initialization process only
